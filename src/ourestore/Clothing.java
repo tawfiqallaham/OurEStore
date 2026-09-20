03476ebd0f8cf0d3ae61d3689918d0e9f0df7b08
yaser <muhammadyaseralmerstani@gmail.com>
2026-09-20 09:08:10 +0400

    build a structure of class customer

--- src/ourestore/Clothing.java
+++ src/ourestore/Clothing.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ 
 package ourestore;
-
-/**
- *
- * @author tawfi
- */
+ 
 public class Clothing extends Product{
     private String brand;
     private Size size;
--- src/ourestore/Customer.java
+++ src/ourestore/Customer.java
@@ -1,14 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ourestore;
 
-/**
- *
- * @author tawfi
- */
-public abstract class Customer {
+package ourestore;
+public abstract class Customer {  
+    private String customerId;   
+    private String name;
+    private String address;
+    private String phone;    
+    private double accountBalance;
+
+    public Customer(String customerId, String name, String address, String phone, double accountBalance) {
+        this.customerId = customerId;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.accountBalance = accountBalance;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+    
+    public void updateBalance(double  amount ){
+        this.accountBalance+=amount;
+    }
+    public abstract double calculatediscount(double totalamount);
+    
+    void  displayCusomerInfo(){
+        System.out.println("Customer ID: "+customerId);        
+        System.out.println("Name: "+name);
+        System.out.println("Address: "+address);
+        System.out.println("Phone: "+phone);
+        System.out.println("Balance: "+accountBalance);
+
+    }
+
     
 }
